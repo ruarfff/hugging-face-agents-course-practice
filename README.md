17d1320dde4198b45cadb37b9b104f2aabc41526
Ruairí O'Brien <rtob@pm.me>
2025-05-25 20:23:26 -0500

    Add initial hugging face use example

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hugging-face-agents-course-practice
+
+Need to get a hugging face token to use the hugging face API <https://huggingface.co/settings/tokens>.
+
+Save it to an environment variable called `HF_TOKEN`:
+
+```bash
+export HF_TOKEN=your_token_here
+```
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from huggingface_hub import InferenceClient
+
+
 def main():
     print("Hello from hugging-face-agents-course-practice!")
+
+    client = InferenceClient("meta-llama/Llama-3.3-70B-Instruct")
+    messages = [
+        {"role": "user", "content": "The capital of France is"}
+    ]
+    output = client.chat_completion(
+        messages=messages,
+        max_tokens=100,
+    )
+
+    print(output.choices[0].message.content)
 
 
 if __name__ == "__main__":
